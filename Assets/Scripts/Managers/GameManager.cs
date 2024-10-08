aece01bdda18633c4fe2936b4863a78fb41071cc
Yuto <yuto_1007@yahoo.ne.jp>
2024-10-08 17:59:38 +0900

    ゲームマネージャーを扱った画面の変更
    今回で画面の文字変更とArea側の画像変更はできたため、残りの音声データ変更を明日の内に取り組む。
    Area側の変更に若干の不備があるため、それも今週の内に修正するようにしたい。

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -8,6 +8,15 @@ public class GameManager : MonoBehaviour
     //instance生成
     public static GameManager instance = null;
 
+    //==SCV数値受け渡し関連=========================================================
+    public string questionContents;
+    public int animationNumber;
+    public int voiceNumber;
+    public int pictureNumber;
+    public string answerContents;
+    //==============================================================================
+
+
     private void Awake()
     {
         if(instance == null)
@@ -21,5 +30,60 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    //==Getter=============================================================================
+
+    //SCVデータ受け取り
+    static public string GetQuestion()
+    {
+        return instance.questionContents;
+    }
+
+    static public int GetAnimation()
+    {
+        return instance.animationNumber;
+    }
+
+    static public int GetVoice()
+    {
+        return instance.voiceNumber;
+    }
+
+    static public int GetPicture()
+    {
+        return instance.pictureNumber;
+    }
+
+    static public string GetAnswer()
+    {
+        return instance.answerContents;
+    }
+
+    //==Setter=============================================================================
+
+    //SCVデータ受け渡し
+    static public void SetQuestion(string value)
+    {
+        instance.questionContents = value;
+    }
+
+    static public void SetAnimation(int value)
+    {
+        instance.animationNumber = value;
+    }
+
+    static public void SetVoice(int value)
+    {
+        instance.voiceNumber = value;
+    }
+
+    static public void SetPicture(int value)
+    {
+        instance.pictureNumber = value;
+    }
+
+    static public void SetAnswer(string value)
+    {
+        instance.answerContents = value;
+    }
     
 }
